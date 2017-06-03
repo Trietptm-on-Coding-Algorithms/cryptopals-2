5700d84d659f5487df74a241d78523d86d0bc384
dweber3 <pennwe@gmail.com>
2017-06-03 17:54:55 -0400

    Added explanatory comments to 2.

--- set1/2.fixedxor.c
+++ set1/2.fixedxor.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-static const unsigned char hex2dec[] = {
+/* This strategy comes courtesy of Orwellophile on stackoverflow. Source:
+ * stackoverflow.com/questions/10324/convert-a-hexaeecimal-string-to-an-integer-efficiently-in-c
+ * This allows direct lookups, speeding up processing time.
+ */
+static const unsigned char hextable[] = {
   [0 ... 255] = 0,
   ['0'] = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
   ['A'] = 10, 11, 12, 13, 14, 15,
   ['a'] = 10, 11, 12, 13, 14, 15
-}; 
-/*courtesy of Orwellophile on stackoverflow
- * stackoverflow.com/questions/10324/convert-a-hexaeecimal-string-to-an-integer-efficiently-in-c
- */
-
+};
 static const unsigned char dec2hex[] = {
   [0] = '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
   'a', 'b', 'c', 'd', 'e', 'f'
 };
 
 /**
- * This takes a hexadecimal number as input and prints its base64 representation.
+ * This takes two hexadecimal strings, decodes them into numbers, then XORS them.
  */
 int main (int argc, char* argv[]) {
 
@@ -28,12 +28,14 @@ int main (int argc, char* argv[]) {
   char output[361] = "";
   char *curA = bufferA;
   char *curB = bufferB;
+  //hexA and hexB are the numeric representations of a character from strings
+  //bufferA and bufferB, respectively.
   unsigned char hexA = 0;
   unsigned char hexB = 0;
   unsigned char hexout = 0;
   unsigned int outindex = 0;
 
-  //input hex value
+  //input hex value as either an argument or stdin.
   if (argc >= 3) {
     strncpy(bufferA, argv[1], sizeof(bufferA));
     bufferA[sizeof(bufferA) - 1] = '\0';
@@ -59,16 +61,20 @@ int main (int argc, char* argv[]) {
     return status;
   }
   
-  int breakpoint = 0;
+  //While both of the characters wwe're looking at aren't null:
   while (*curA && *curB) {
+    //First we convert the characters to numbers.
     hexA = hex2dec[*curA];
     hexB = hex2dec[*curB];
+    //Then we XOR them.
     hexout = hexA ^ hexB;
+    //Third, we encode the output.
     output[outindex++] = dec2hex[hexout];
+    //Finally, we increment our counters.
     curA++;
     curB++;
-  } 
-  //print result
+  }
+  //Output is now ready to be served.
   printf("%s\n", output);
 
   return status;
